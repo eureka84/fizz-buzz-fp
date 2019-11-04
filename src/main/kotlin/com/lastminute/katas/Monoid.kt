49e22bb93fdc686a70ae6a3e345d917c80daa1ea
asciarra <angelo.sciarra@lastminute.com>
2019-11-04 13:59:04 +0100

    added delegation

--- src/main/kotlin/com/lastminute/katas/Monoid.kt
+++ src/main/kotlin/com/lastminute/katas/Monoid.kt
@@ -10,27 +10,26 @@ val semigroupString: Semigroup<String> = object : Semigroup<String> {
     override fun combine(a: String, b: String): String = a + b
 }
 
-fun <A> semigroupOption(semigroupA: Semigroup<A>): Semigroup<Option<A>> = object : Semigroup<Option<A>> {
-    override fun combine(a: Option<A>, b: Option<A>): Option<A> =
-        a.fold(
-            { b },
-            { valA: A ->
-                b.fold(
-                    { a },
-                    { valB: A ->
-                        Option.just(semigroupA.combine(valA, valB))
-                    }
-                )
-            }
-        )
-}
+fun <A> semigroupOption(semigroupA: Semigroup<A>): Semigroup<Option<A>> =
+    object : Semigroup<Option<A>> {
+        override fun combine(a: Option<A>, b: Option<A>): Option<A> =
+            a.fold(
+                { b },
+                { valA: A ->
+                    b.fold(
+                        { a },
+                        { valB: A ->
+                            Option.just(semigroupA.combine(valA, valB))
+                        }
+                    )
+                }
+            )
+    }
 
-fun <A> monoidOption(semigroupOption: Semigroup<Option<A>>): Monoid<Option<A>> = object : Monoid<Option<A>> {
-    override fun combine(a: Option<A>, b: Option<A>): Option<A> =
-        semigroupOption.combine(a, b)
-
-    override fun empty(): Option<A> = Option.empty()
-}
+fun <A> monoidOption(semigroupOption: Semigroup<Option<A>>): Monoid<Option<A>> =
+    object : Monoid<Option<A>>, Semigroup<Option<A>> by semigroupOption {
+        override fun empty(): Option<A> = Option.empty()
+    }
 
 interface Monoid<A> : Semigroup<A> {
     fun empty(): A

6277e7d12445ea4ee35a72c33a3596a33408a734
asciarra <angelo.sciarra@lastminute.com>
2019-11-05 09:28:43 +0100

    minor refactor

--- src/main/kotlin/com/lastminute/katas/Monoid.kt
+++ src/main/kotlin/com/lastminute/katas/Monoid.kt
@@ -6,6 +6,10 @@ interface Semigroup<A> {
     fun combine(a: A, b: A): A
 }
 
+interface Monoid<A> : Semigroup<A> {
+    fun empty(): A
+}
+
 val semigroupString: Semigroup<String> = object : Semigroup<String> {
     override fun combine(a: String, b: String): String = a + b
 }
@@ -30,10 +34,6 @@ fun <A> monoidOption(semigroupOption: Semigroup<Option<A>>): Monoid<Option<A>> =
     object : Monoid<Option<A>>, Semigroup<Option<A>> by semigroupOption {
         override fun empty(): Option<A> = Option.empty()
     }
-
-interface Monoid<A> : Semigroup<A> {
-    fun empty(): A
-}
 
 // This can be abstracted for all structure that supports fold
 fun <A, B> List<A>.foldMap(m: Monoid<B>, f: (A) -> B): B = this.fold(m.empty()) { acc, curr -> m.combine(acc, f(curr)) }
